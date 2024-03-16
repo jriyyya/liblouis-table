6e7bf1ff16491c0f57dd462dd20f8a055ab3cd13
Riya Jain <riya1jain567@gmail.com>
2024-03-16 20:18:52 +0530

    opcode selector

--- src/components/UnicodeSelector.py
+++ src/components/UnicodeSelector.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from utils.QtAsync import AsyncTask, coroutine
 
-class UnicodeViewer(QWidget):
+class UnicodeSelector(QWidget):
     def __init__(self, parent = None):
         super().__init__(parent)
 
@@ -16,17 +18,23 @@ class UnicodeViewer(QWidget):
         self.unicodes_list = QListWidget()
         self.unicodes_list.itemClicked.connect(lambda x : search.setText(x.text()))
 
-        for code in range(0x0000, 0xFFFF):
+        layout.addWidget(search)
+        layout.addWidget(label)
+        layout.addWidget(self.unicodes_list)
+
+        self.populate_list()
+
+        self.setLayout(layout)
+
+    def populate_list(self):
+        def addCode(code):
             txt = f"{chr(code)} (U+{code:04X})"
             item = QListWidgetItem(txt)
             item.setToolTip(f"Select {txt}")
             self.unicodes_list.addItem(item)
 
-        layout.addWidget(search)
-        layout.addWidget(label)
-        layout.addWidget(self.unicodes_list)
-
-        self.setLayout(layout)
+        for code in range(0x0000, 0xFFFF):
+            addCode(code)
 
     def on_select(self, cb):
         def callback(code):
--- src/main.py
+++ src/main.py
@@ -1,19 +1,26 @@
 import sys
 from PyQt5.QtWidgets import *
-from components.UnicodeViewer import UnicodeViewer
+from PyQt5.QtCore import *
+from components.UnicodeSelector import UnicodeSelector
+from components.OpcodeSelector import OpcodeSelector
 from components.ButtonTextInput import ButtonTextInput
 
-opcodes = ["punctuation", "identifier", "word", "math", "number"]
-
-class TableCreator(QWidget):
+class TableManager(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle('Liblouis Table Creator')
+        self.setWindowTitle('Liblouis Tables Manager')
 
         self.popup = None
 
         layout = QVBoxLayout()
+
+
+        self.input_opcode = ButtonTextInput()
+        self.input_opcode.input.setReadOnly(True)
+        self.input_opcode.input.setPlaceholderText("Select Opcode")
+        self.input_opcode.button.setText("Select")
+        self.input_opcode.button.clicked.connect(lambda : self.showOpcodePopup())
 
         self.input_unicode = ButtonTextInput()
         self.input_unicode.input.setReadOnly(True)
@@ -21,30 +28,29 @@ class TableCreator(QWidget):
         self.input_unicode.button.setText("Select")
         self.input_unicode.button.clicked.connect(lambda : self.showUnicodePopup())
 
-        self.input_opcode = QTextEdit()
         self.input_braille = QTextEdit()
 
 
-        layout.addWidget(self.input_unicode)
         layout.addWidget(self.input_opcode)
+        layout.addWidget(self.input_unicode)
         layout.addWidget(self.input_braille)
 
         self.setLayout(layout)
 
-    def showUnicodePopup(self):
-        self.popup = UnicodeViewer()
+    def showOpcodePopup(self):
+        self.popup = OpcodeSelector()
         self.popup.on_select(lambda code : self.input_unicode.input.setText(code) )
         self.popup.show()
-    # def openPopup(self):
-    #     popup = PopupDialog(self)
-    #     popup.setGeometry(self.geometry().center().x() - 150, self.geometry().center().y() - 100, 300, 200)  # Center the popup
-    #     popup.add_content(QPushButton("Close", self))
-    #     popup.show()
 
+    def showUnicodePopup(self):
+        self.popup = UnicodeSelector()
+        self.popup.on_select(lambda code : self.input_unicode.input.setText(code) )
+        self.popup.show()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = TableCreator()
-    # window.setGeometry(300, 200, 800, 400)
+
+    window = TableManager()
     window.show()
+
     sys.exit(app.exec_())
